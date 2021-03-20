96f9f431595547ec2b72ed0dbcaa0eb7458009ef
Zvat Scorobogatov <zvat.333333@gmail.com>
2021-03-20 10:18:50 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd6f023c6e62e720de81bb39be03573df'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '405504d2-7802-4ef8-8063-2b764c340e8b'
+};
